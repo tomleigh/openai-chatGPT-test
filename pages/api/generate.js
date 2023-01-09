ce18c0a1d64ae987312e28d30e796000552d7b77
Tom Leigh <tomleigh2000@gmail.com>
2023-01-09 18:05:51 +1100

    Initial commit

---
+++ pages/api/generate.js
@@ -0,0 +1,62 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message: "OpenAI API key not configured, please follow instructions in README.md",
+      }
+    });
+    return;
+  }
+
+  const animal = req.body.animal || '';
+  if (animal.trim().length === 0) {
+    res.status(400).json({
+      error: {
+        message: "Please enter a valid animal",
+      }
+    });
+    return;
+  }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(animal),
+      temperature: 0.6,
+      max_tokens: 1024,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch(error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: 'An error occurred during your request.',
+        }
+      });
+    }
+  }
+}
+
+function generatePrompt(animal) {
+  const capitalizedAnimal =
+    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
+  return `Describe the person ${capitalizedAnimal}.
+  
+  Tom Leigh: Tom Leigh is a student at the University of Sydney Australia. He is 22 years old, and studies a Bachelor of Advanced Computing and a Bachelor of Commerce. Tom is passionate about technology, fitness, surfing, and guitar. Tom is taking a trip to Nepal to climb Everest Base Camp in January 2023.
+
+  Mark Twain: Mark Twain is a famous American author, philosopher and poet. He is renowned around the world for his insightful thoughts and ability to summarise meaningful statements into short sentences.
+  
+  ${capitalizedAnimal}: `;
+}
